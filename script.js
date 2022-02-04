a0c64401ccd87198d0ffb049e63545bc04d8c9ab
Emirhan Pala <emrhnpla02@protonmail.com>
2022-02-04 14:38:55 +0300

    init 🌑

---
+++ script.js
@@ -0,0 +1,34 @@
+const terminals = document.querySelectorAll(".terminal"),
+  commandSpan = document.querySelector("#command"),
+  command = "epfetch";
+
+setTimeout(() => {
+  let j = 0;
+  terminals[0].children[1].classList = "cursor";
+  const commandloop = setInterval(() => {
+    commandSpan.innerText += command[j++];
+    if (j >= command.length) {
+      epfetch();
+      clearInterval(commandloop);
+    }
+  }, 200);
+}, 2000);
+
+const epfetch = () => {
+  const items = document.querySelectorAll(".fetchitem"),
+    paletteDiv = document.querySelector("#_palette");
+
+  terminals[0].children[1].classList = "";
+
+  let i = 0;
+  const loop = setInterval(() => {
+    items[i++].style.display = "block";
+    if (i >= items.length) clearInterval(loop);
+  }, 50);
+
+  setTimeout(() => {
+    paletteDiv.style.display = "grid";
+    terminals[1].style.display = "block";
+    terminals[1].children[1].classList = "cursor blink";
+  }, 50 * items.length);
+};
